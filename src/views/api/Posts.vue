6934d11522a3d1fcd37307cd262123b4d5a34b17
257lad <isaacndayize@gmail.com>
2026-05-15 20:14:51 +0200

    POST METHOD EXAMPLE

--- src/views/api/Posts.vue
+++ src/views/api/Posts.vue
@@ -1,31 +1,94 @@
 <template>
   <h2>Posts - GET ({{ data?.data?.length }})</h2>
-  <div v-for="item in data?.data" :key="item.id">
-    <h3>{{ item.title }}</h3>
-    <p>{{ item.body }}</p>
-    <hr>
+
+  <div class="container">
+    <div class="posts">
+      <div v-for="item in data?.data" :key="item.id">
+        <h3>{{ item.title }}</h3>
+        <p>{{ item.body }}</p>
+        <hr>
+      </div>
+    </div>
+    <form action="" class="form" @submit.prevent="submitPost">
+      <h3>POSTING FORM</h3>
+      <div>
+        <label for="">User ID</label>
+        <input type="number" v-model="userId">
+      </div>
+      <br>
+      <div>
+        <label for="">Title</label>
+        <input type="text" v-model="title">
+      </div>
+      <br>
+      <div>
+        <label for="">Body</label>
+        <input type="text" v-model="body">
+      </div>
+      <br>
+      <br>
+      <button>POST</button>
+    </form>
   </div>
 </template>
 <script>
 export default {
-  data(){
+  data() {
     return {
-      data: {}
+      data: {},
+
+      userId: '',
+      title: '',
+      body: '',
     }
   },
   methods: {
-    getPosts(){
-
+    getPosts() {
       axios.get('posts/')
-      .then((rep) => {
-        this.data = rep
-      }).catch((error)=> {
-        console.log(error)
-      })
+        .then((rep) => {
+          this.data = rep
+        }).catch((error) => {
+          console.log(error)
+        })
+    },
+    submitPost(){
+      const data = {
+        "userId":this.userId,
+        // "id": 1,
+        "title": this.title,
+        "body": this.body
+      }
+      axios.post('posts/', data)
+        .then((response)=> {
+          console.log(response);
+          
+        }).catch((error)=> {
+          console.log(error);
+          
+        })
     }
   },
-  mounted(){
+  mounted() {
     this.getPosts()
   }
 }
 </script>
+
+<style>
+.container {
+  position: relative;
+  display: flex;
+  align-items: start;
+  gap: 30px;
+
+  form{
+    position: sticky;
+    top: 0;
+
+    button {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+</style>
